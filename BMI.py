ffee50ea04247a22dd1f81ee13da198dfa6c7115
Hollis515 <Hollis20110515@gmail.com>
2026-09-12 15:38:58 +0800

    Add BMI category

--- BMI.py
+++ BMI.py
@@ -1,2 +1,12 @@
 def calculate_bmi(weight, height):
     return weight / (height ** 2)
+
+def get_bmi_category(bmi):
+    if bmi < 18.5:
+        return "體重過輕"
+    elif 18.5 <= bmi < 25:
+        return "正常體重"
+    elif 25 <= bmi < 30:
+        return "體重過重"
+    else:
+        return "肥胖"
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from bmi import calculate_bmi
+from BMI import calculate_bmi, get_bmi_category
 
 weight=float(input("請輸入你的體重(kg):"))
 height=float(input("請輸入你的升高(cm):"))
 
 bmi=calculate_bmi(weight,height/100)
+category=get_bmi_category(bmi)
 
 print(f"你的BMI為:{bmi:.2f}")
+print(f"你的BMI分類為:{category}")
